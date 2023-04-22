0c8a3afaef4f8898d29bdb57a8304084179ee976
Grant Lonie <grantlonie@gmail.com>
2023-04-21 22:40:41 -0600

    chore: update react dep and remove tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from "react"
 
-import SingleSelection from './SingleSelection'
-import Carousel from './Carousel'
-import Vertical from './Vertical'
-import Responsive from './Responsive'
-import Controlled from './Controlled'
+import SingleSelection from "./SingleSelection"
+import Carousel from "./Carousel"
+import Vertical from "./Vertical"
+import Responsive from "./Responsive"
+import Controlled from "./Controlled"
 
 export default class App extends Component {
 	render() {
 		const pageStyle = {
-			maxWidth: '800px',
-			margin: 'auto',
-			fontFamily: 'Calibri',
+			maxWidth: "800px",
+			margin: "auto",
+			fontFamily: "Calibri",
 		}
 
 		const selectionStyle = {
-			height: this.props.pageHeight + 'px',
-			width: this.props.pageWidth + 'px',
-			backgroundColor: 'gray',
-			display: 'inline-block',
-			border: '3px solid white',
-			color: 'white',
-			textAlign: 'center',
-			verticalAlign: 'middle',
-			lineHeight: this.props.pageHeight + 'px',
-			userSelect: 'none',
-			fontFamily: 'calibri',
+			height: this.props.pageHeight + "px",
+			width: this.props.pageWidth + "px",
+			backgroundColor: "gray",
+			display: "inline-block",
+			border: "3px solid white",
+			color: "white",
+			textAlign: "center",
+			verticalAlign: "middle",
+			lineHeight: this.props.pageHeight + "px",
+			userSelect: "none",
+			fontFamily: "calibri",
 		}
 
 		return (
@@ -40,7 +40,7 @@ export default class App extends Component {
 				<Vertical selectionStyle={selectionStyle} />
 				<Responsive selectionStyle={selectionStyle} />
 				<Controlled selectionStyle={selectionStyle} />
-				<div style={{ display: 'inline-block', height: '200px' }} />
+				<div style={{ display: "inline-block", height: "200px" }} />
 			</div>
 		)
 	}
