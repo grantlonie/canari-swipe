f5d5c965c524d916533f03f96b641a7527b60038
Grant Lonie <grantlonie@gmail.com>
2023-07-11 22:14:24 -0600

    chore: add source maps to app

--- build-config/rollup.config.js
+++ build-config/rollup.config.js
@@ -3,7 +3,7 @@ import { babel } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import remarkGfm from 'remark-gfm'; // Tables, footnotes, strikethrough, task lists, literal URLs.
+import remarkGfm from 'remark-gfm' // Tables, footnotes, strikethrough, task lists, literal URLs.
 import css from 'rollup-plugin-import-css'
 import livereload from 'rollup-plugin-livereload'
 import globals from 'rollup-plugin-node-globals'
@@ -14,7 +14,7 @@ const directory = production ? 'build' : 'dev'
 
 export default {
 	input: './src/index.js',
-	output: { dir: directory, format: 'es' },
+	output: { dir: directory, format: 'es', sourcemap: true },
 	plugins: [
 		babel({
 			babelHelpers: 'bundled',
